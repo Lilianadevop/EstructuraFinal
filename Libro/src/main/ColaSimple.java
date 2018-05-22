27b8acc924352c38c09cd734e05aa5cc75b73a0b
Lilianadevop <36047405+Lilianadevop@users.noreply.github.com>
2018-05-22 17:26:15 -0600

    es la de libro
    pero es el de libro

---
+++ Libro/src/main/ColaSimple.java
@@ -0,0 +1,66 @@
+package main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alondriwis
+ */
+ import static javax.swing.JOptionPane.*;
+public class ColaSimple {
+    private int tam,tope, A[];
+
+    public ColaSimple() {
+    tam=0;
+    tope=-1;
+    }
+
+    public ColaSimple(int t) {
+        tam=t;
+        tope=-1;
+        A=new int[tam];
+    }//constructor
+    
+    
+    public boolean llena(){
+    
+    return tope==tam-1;
+    }
+    public boolean vacia(){
+    return tope==-1;
+    }
+    
+    
+    
+
+   public void meter(int d){
+   if(!llena()){
+   A[++tope]=d;
+   
+   }
+   else {
+       showMessageDialog(null,"llena");
+   }
+   } ///fin metodo meter 
+    
+   
+    public int sacar(){
+   int s=A[0];
+    for(int i=0;i<tam-1;i++)  A[i]=A[i+1];
+    tope--;
+    return s;
+   }
+    //}//metodo sacar
+    public int getTope(){ 
+        return tope;
+           }
+
+    public int getTam() {
+        return tam;
+    }
+
+}
